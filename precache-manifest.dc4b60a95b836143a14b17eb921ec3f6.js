db1f75c01cd0bbefc1c4a4dc9d74e01042c5bbfc
Makos <makos4608@gmail.com>
2020-11-25 14:41:04 +0100

    Updates

---
+++ precache-manifest.dc4b60a95b836143a14b17eb921ec3f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e813ffe7df90eadd777c22bd96e55786",
+    "url": "/clothesShop/index.html"
+  },
+  {
+    "revision": "4af01878e4469e7c281f",
+    "url": "/clothesShop/static/js/2.86782b1e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/clothesShop/static/js/2.86782b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93695395af34a29500c",
+    "url": "/clothesShop/static/js/main.9d139ebe.chunk.js"
+  },
+  {
+    "revision": "692e6891b993851f2f26",
+    "url": "/clothesShop/static/js/runtime-main.2f43776c.js"
+  },
+  {
+    "revision": "ab2eb5904a224fc6c2c175c9a550d623",
+    "url": "/clothesShop/static/media/arrow_down.ab2eb590.svg"
+  },
+  {
+    "revision": "52fa90c2075551dcc12380a78dcdd657",
+    "url": "/clothesShop/static/media/arrow_forward.52fa90c2.svg"
+  },
+  {
+    "revision": "797146eca6615d6cadd63b0e5bf1068a",
+    "url": "/clothesShop/static/media/baner.797146ec.webp"
+  },
+  {
+    "revision": "16793e816beb3255cd81812f58231bff",
+    "url": "/clothesShop/static/media/delete.16793e81.svg"
+  },
+  {
+    "revision": "92521a1228199123882e9f8bc0f6d09d",
+    "url": "/clothesShop/static/media/search.92521a12.svg"
+  },
+  {
+    "revision": "82daa34cb574ba0b99c5cbcda1808641",
+    "url": "/clothesShop/static/media/shopping_cart.82daa34c.svg"
+  }
+]);
